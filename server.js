af524d0ce4744c995f80d14e2bd3459aa32e37a0
Alvaro Concha <alvaroconcha14@gmail.com>
2025-10-30 13:10:23 -0300

    1.3

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 const SUPABASE_URL = process.env.SUPABASE_URL;
